ab48ce42a4dff49e22aacb55dd88136a467810eb
Raith Hamzah <rhamzah@magento.com>
2017-12-03 11:58:13 -0600

    Item Pickup

---
+++ Assets/scripts/ItemPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Pickup
+{
+    public GameController game;
+    public Item item;
+
+    private void Start()
+    {
+        game = GameObject.FindGameObjectWithTag("GameController")
+            .GetComponent<GameController>();
+    } 
+
+    public override void OnPickup()
+    {
+        game.OnItemPickup(item);
+    }
+}
---
+++ Assets/scripts/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pickup : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!other.CompareTag("Player"))
+		{
+			return;
+		}
+		OnPickup();
+		Destroy(gameObject);
+		
+	}
+	
+	// What to do before the object is destroyed
+	abstract public void OnPickup();
+}
